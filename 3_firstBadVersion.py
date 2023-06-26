fd142ba6a6428338939e3c3615676218ba673971
Jaya Vardhan Swarna <79640790+Jayavardhan-swarna@users.noreply.github.com>
2023-06-26 09:42:17 +0530

    Create 3_firstBadVersion.py

---
+++ 3_firstBadVersion.py
@@ -0,0 +1,13 @@
+def isBadVersion(version):
+    # Implementation of the isBadVersion API function
+    # This function checks if a given version is bad
+    pass
+
+
+
+
+n = 5
+bad = 4
+
+first_bad = firstBadVersion(n)
+print("First bad version:", first_bad)
